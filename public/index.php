ccf757a6c2a322166dc01e0822f625074f46f39b
Tritchaphorn BOISSON <tritchaphorn@gmail.com>
2021-12-15 18:35:09 +0100

    first commit

--- public/index.php
+++ public/index.php
@@ -3,7 +3,10 @@
 // Get a $twig object from this file.
 require_once __DIR__ . '/../config/twig.php';
 
+require_once __DIR__ . '/../public/product.php';
+
 
 $name = 'Wilder';
 
-echo $twig->render('home.html.twig', ['name' => $name]);
+
+echo $twig->render('products.html.twig', ['products' => $products]);
---
+++ public/product.php
@@ -0,0 +1,4 @@
+<?php
+
+$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
+
